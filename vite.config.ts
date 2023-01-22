9a81fbe326341572b3ae43d3fbee8ecf025961b7
Danny Speckmann <danny.speckmann@elise.de>
2023-01-22 18:20:10 +0100

    Fix build by adding topLevelAwait plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -39,7 +40,9 @@ export default defineConfig({
           },
         ],
       }
-    })],
+    }),
+    topLevelAwait()
+  ],
   server: {
     proxy: {
       '/api': {
